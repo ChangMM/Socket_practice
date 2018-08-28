9e3914ee5b3276cde13570b7c1b644966f2d32a5
ChangMM <1769111903@qq.com>
2018-08-28 20:04:00 +0800

    add decoration with args

---
+++ decoration/decoration_with_args.py
@@ -0,0 +1,55 @@
+from functools import wraps
+import logging
+import time
+
+
+# 带参数的装饰器
+def logged(level, name = None, message = None):
+	"""
+	    Add logging to a function. level is the logging
+	    level, name is the logger name, and message is the
+	    log message. If name and message aren't specified,
+	    they default to the function's module and name.
+	"""
+	def decoration(func):
+		log_name = name if name else func.__module__
+		log_message = message if message else func.__name__
+		log = logging.getLogger(log_name)
+		
+		@wraps(func)
+		def wrapper(*args, **kargs):
+			log.log(level, log_message)
+			print(level, log_message)
+			return func(*args, **kargs)
+		return wrapper
+	return decoration
+
+
+# 不带参数的装饰器
+def time_cal(func):
+	@wraps(func)
+	def wrapper(*args, **kargs):
+		start = time.time()
+		result = func(*args, **kargs)
+		end = time.time()
+		print("The result is: ", result, ", cost time: ", end - start)
+		return result
+	return wrapper
+
+
+@logged(logging.DEBUG)
+def test_log():
+	print("This is a function to test logged decoration")
+
+
+@time_cal
+def test_time_cal():
+	temp = 0
+	for i in range(1000000):
+		temp += i
+	return temp
+
+
+if __name__ == "__main__":
+	test_log()
+	test_time_cal()
